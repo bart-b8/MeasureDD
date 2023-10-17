e8b9b8085c03e271eac0ad809974908c54c29453
bart-b8 <68114296+bart-b8@users.noreply.github.com>
2023-10-17 17:55:20 +0200

    improve context

--- MeasureDD.Requests/Context.cs
+++ MeasureDD.Requests/Context.cs
@@ -3,18 +3,18 @@ namespace MeasureDD;
 public static class Context
 {
   // public const string docPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-  const string docPath = "C:/Users/bartd/Documents/0. Files/SoftwareDevelopment/Projects/MeasureDD";
-  const string filePath = "files/requests.txt" ;
-  const string testFilePath = "MeasureDD.Tests/files/requests.txt";
+  const string docPath = @"C:\Users\bartd\Documents\0. Files\SoftwareDevelopment\Projects\MeasureDD\";
+  const string filePath = @"files/requests.txt" ;
+  const string testFilePath = @"MeasureDD.Tests/files/requests.txt";
 
   public static string GetTestFilePath()
   {
-    return docPath + testFilePath;
+    return Path.Combine(docPath, testFilePath);
   }
 
   public static string GetFilePath()
   {
-    return docPath + filePath;
+    return Path.Combine(docPath, filePath);
   }
 
 }
