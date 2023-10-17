b9ea4c6906d726d3be4001bb8d6b72d44773241f
bart-b8 <68114296+bart-b8@users.noreply.github.com>
2023-10-17 14:07:18 +0200

    remove circular project references - move context to request

---
+++ MeasureDD.Requests/Context.cs
@@ -0,0 +1,20 @@
+namespace MeasureDD;
+
+public static class Context
+{
+  // public const string docPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+  const string docPath = "C:/Users/bartd/Documents/0. Files/SoftwareDevelopment/Projects/MeasureDD";
+  const string filePath = "files/requests.txt" ;
+  const string testFilePath = "MeasureDD.Tests/files/requests.txt";
+
+  public static string GetTestFilePath()
+  {
+    return docPath + testFilePath;
+  }
+
+  public static string GetFilePath()
+  {
+    return docPath + filePath;
+  }
+
+}
--- MeasureDD/Context.cs
+++
@@ -1,6 +0,0 @@
-namespace MeasureDD;
-
-public static class Context
-{
-  public const string filePath = "./files/requests.txt" ;
-}
